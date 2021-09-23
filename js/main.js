c808dbab02239b477f34eafb4d52fee0eec72f3b
ComradEvg <88490590+ComradEvg@users.noreply.github.com>
2021-09-23 23:54:46 +0300

    ver 0.5

--- js/main.js
+++ js/main.js
@@ -1,54 +1,49 @@
-$(document).ready(function () {
 
-   function ibg() {
-      $.each($('.ibg'), function (index, val) {
-         if ($(this).find('img').length > 0) {
-            $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
-         }
-      });
+
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".menu");
+burger.addEventListener('click', function () {
+   burger.classList.toggle("active");
+   if (burger.classList.contains("active")) {
+      nav.classList.add("active");
    }
-   ibg();
-
-   $('.burger').click(function (event) {
-      $('.burger,.menu,.link__lang').toggleClass('active');
-      $('body').toggleClass('lock');
-   });
-
-   $('.slider__row').slick({
-      dots: false,
-      autoplay: false,
-      autoplaySpeed: 1000,
-      infinite: true,
-      speed: 500,
-      fade: true,
-      cssEase: 'linear',
-      nextArrow: '<div class="arrow-right-item"><svg class="arrow-right"><use xlink: href = "#arrow-right"></use></svg></div>',
-      prevArrow: '<div class="arrow-left-item"><svg class="arrow-left"><use xlink: href ="#arrow-left"></use></svg></div>',
-      appendArrows: $('.container-arrows'),
-   });
-
-   var ffff = $(window).width();
-
-   if (ffff <= 767) {
-      $('.servises__row').slick({
-         dots: true,
-         autoplay: false,
-         autoplaySpeed: 1000,
-         infinite: true,
-         speed: 500,
-         fade: true,
-         cssEase: 'linear',
-         arrows: false,
-      });
-   } else {
+   else {
+      nav.classList.remove("active");
    }
-
-   $('.card').hover(function (event){
-      $(this).toggleClass('active-card');
-   })
-
-   $('.flip').on('click', function (event) {
-      $(this).toggleClass('active-flip');
-      $('.finger').addClass('finger-none');
-   })
 });
+var mousestart;
+var touches;
+var touchess
+var mousemove;
+nav.addEventListener('touchstart', function (e) {
+   TouchStart(e);
+   console.log(touches);
+});
+nav.addEventListener('touchmove', function (e) {
+   TouchMove(e);
+   console.log(mousemove);
+});
+function TouchStart(e){
+   touches = e.changedTouches;
+   mousestart = e.targetTouches[0].clientX;
+
+};
+function TouchMove(e){
+   e.preventDefault();
+   touchess = e.changedTouches;
+   mousemove = e.targetTouches[0].clientX;
+   if (mousestart+100 < mousemove){
+         nav.classList.remove("active");
+         burger.classList.remove("active");
+      }
+};
+
+function ibgmy() {
+   const ibgs = document.getElementsByClassName('ibg');
+   for (let ibg of ibgs) {
+      const img = ibg.querySelector("img");
+      const imgsrc = img.getAttribute("src");
+      ibg.style.backgroundImage = 'url("' + imgsrc + '")';
+   }
+}
+ibgmy()
