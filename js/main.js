68dd55ef8a3312c299591e2bbbc37bd2c1ac21cc
ComradEvg <88490590+ComradEvg@users.noreply.github.com>
2021-10-01 12:56:27 +0300

    ver 0.6

--- js/main.js
+++ js/main.js
@@ -1,43 +1,4 @@
 
-
-const burger = document.querySelector(".burger");
-const nav = document.querySelector(".menu");
-burger.addEventListener('click', function () {
-   burger.classList.toggle("active");
-   if (burger.classList.contains("active")) {
-      nav.classList.add("active");
-   }
-   else {
-      nav.classList.remove("active");
-   }
-});
-var mousestart;
-var touches;
-var touchess;
-var mousemove;
-nav.addEventListener('touchstart', function (e) {
-   TouchStart(e);
-   console.log(touches);
-});
-nav.addEventListener('touchmove', function (e) {
-   TouchMove(e);
-   console.log(mousemove);
-});
-function TouchStart(e){
-   touches = e.changedTouches;
-   mousestart = e.targetTouches[0].clientX;
-
-};
-function TouchMove(e){
-   e.preventDefault();
-   touchess = e.changedTouches;
-   mousemove = e.targetTouches[0].clientX;
-   if (mousestart+100 < mousemove){
-         nav.classList.remove("active");
-         burger.classList.remove("active");
-      }
-};
-
 function ibgmy() {
    const ibgs = document.getElementsByClassName('ibg');
    for (let ibg of ibgs) {
---
+++ js/menu.js
@@ -0,0 +1,47 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".menu");
+burger.addEventListener('click', function () {
+   burger.classList.toggle("active");
+   if (burger.classList.contains("active")) {
+      nav.classList.add("active");
+   }
+   else {
+      nav.classList.remove("active");
+   }
+});
+
+document.addEventListener('click', function (e) {
+   const target = e.target;
+   const targetMenu = target == nav || nav.contains(target);
+   const targetBurger = target == burger;
+   const menuActive = nav.classList.contains('active');
+   
+
+   if (!targetMenu && !targetBurger && menuActive) {
+      nav.classList.remove("active");
+      burger.classList.remove("active");
+   }
+   else {
+      nav.addEventListener('touchstart', function (e) {
+         TouchStart(e);
+      });
+      nav.addEventListener('touchmove', function (e) {
+         TouchMove(e);
+      });
+   }
+});
+let mousestart;
+let mousemove;
+
+function TouchStart(e) {
+   mousestart = e.targetTouches[0].clientX;
+};
+function TouchMove(e) {
+   e.preventDefault();
+   mousemove = e.targetTouches[0].clientX;
+   console.log(mousemove);
+   if (mousestart + 100 < mousemove) {
+      nav.classList.remove("active");
+      burger.classList.remove("active");
+   }
+};
